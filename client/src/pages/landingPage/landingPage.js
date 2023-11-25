9e830f42fe5bd4f54b6fb2d20300d0b4e21aa591
Jeffrey Wang <jeffrey.19jw@gmail.com>
2023-11-25 17:23:16 -0500

    feat: Landing page
    added landing page html + css. Also included images

--- client/src/pages/landingPage/landingPage.js
+++ client/src/pages/landingPage/landingPage.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import './landing.css';
+import logo from '../../images/outsider-trading-logo.png'
+import graphic from '../../images/raining-money-transparent.png'
+import { NavLink } from 'react-router-dom';
 
 const landingPage = () => {
   return (
-    <div>landingPage</div>
+    <>
+        <div class="landing-container">
+            <div className='text-section'>
+                <img src={logo} alt="OutsiderTrading" id="logo"/>
+                <h1>Senators can regulate firms,</h1>
+                <h1> then invest in those same firms.</h1>
+                <h2>Just saying.</h2>
+
+                <h3>Easily invest in and track hot congressional stocks with our automated tools.</h3>
+                
+                <NavLink to="/portfolio" style={{textDecoration: "none"}} className="butt">Build your portfolio today</NavLink>
+            </div>
+
+            <div className="image-section">
+                <img src={graphic} alt="landing-graphic" id="large-graphic"/>
+            </div>
+        </div> 
+
+    </>
+    
   )
 }
 
